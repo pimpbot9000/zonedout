8c28a758c39991d7323194b9c4ab69d95f145570
Tuomas Välimäki <t.valimaki@gmail.com>
2020-10-14 15:57:26 +0300

    clean faulty custom queries

--- src/main/java/zonedout/repositories/CustomQueryRepo.java
+++ src/main/java/zonedout/repositories/CustomQueryRepo.java
@@ -20,13 +20,7 @@ import zonedout.models.UserAccount;
 public interface CustomQueryRepo extends JpaRepository<ProfileImage, Long> {
     
     @Query(value = "SELECT ID AS id, USERNAME as username, CONTACTS_ID as contactsId FROM USER_ACCOUNT LEFT OUTER JOIN (SELECT * FROM USER_ACCOUNT_CONTACTS  WHERE CONTACTS_ID = 1) AS FRIEND ON USER_ACCOUNT.ID = FRIEND.USER_ACCOUNT_ID",  nativeQuery = true)
-    List<IMyQuery> getWithFriends();  
-    
-    @Query("SELECT id as id, username as username, id as contactsId FROM UserAccount")
-    List<IMyQuery> getWithFriends2();  
-    
-    //, CONTACTS_ID as contactsId
-    @Query("SELECT id AS id, username as username, contactsId as contactsId FROM UserAccount LEFT OUTER JOIN (SELECT * FROM UserAccount u.contacts WHERE contactsId = 1) AS Friend ON UserAccount.If = Friend.UserAccountId")
-    List<IMyQuery> getWithFriends3();  
+    List<IMyQuery> getWithFriends();   
+     
 }
 
